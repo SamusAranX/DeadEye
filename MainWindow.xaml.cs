39b8a3a69ba60148aaeacdbe5346e04f785aac0b
Emma Alyx Wunder <hallo@emmalyx.dev>
2021-01-15 03:55:19 +0100

    First proof of concept screen capture

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -17,7 +17,7 @@ namespace DeadEye {
 	/// <summary>
 	/// Interaction logic for MainWindow.xaml
 	/// </summary>
-	public partial class MainWindow : Window {
+	public partial class MainWindow {
 		public MainWindow() {
 			this.InitializeComponent();
 		}
