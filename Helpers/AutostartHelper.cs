e65bf7743162fea4557fa2e3892ee129797092bd
Peter Wunder <hallo@peterwunder.de>
2021-03-04 12:57:14 +0100

    Added autostart functionality
    Added name to assembly information so it shows up in the task manager

---
+++ Helpers/AutostartHelper.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Reflection;
+using Microsoft.Win32;
+
+namespace DeadEye.Helpers {
+	internal class AutostartHelper {
+		private const string REGISTRY_KEY = "DeadEye";
+		private const string RUN_KEY = @"SOFTWARE\Microsoft\Windows\CurrentVersion\Run";
+
+		public static bool CheckAutostartStatus() {
+			using var registryKey = Registry.CurrentUser.OpenSubKey(RUN_KEY, false);
+			if (registryKey == null)
+				throw new KeyNotFoundException("The Run key does not exist.");
+
+			return registryKey.GetValue(REGISTRY_KEY) != null;
+		}
+
+		public static void EnableAutostart() {
+			using var registryKey = Registry.CurrentUser.OpenSubKey(RUN_KEY, true);
+			if (registryKey == null)
+				throw new KeyNotFoundException("The Run key does not exist.");
+
+			registryKey.SetValue(REGISTRY_KEY, Assembly.GetExecutingAssembly().Location);
+			Debug.WriteLine("Autostart enabled.");
+		}
+
+		public static void DisableAutostart() {
+			using var registryKey = Registry.CurrentUser.OpenSubKey(RUN_KEY, true);
+			if (registryKey == null)
+				throw new KeyNotFoundException("The Run key does not exist.");
+
+			registryKey.DeleteValue(REGISTRY_KEY);
+			Debug.WriteLine("Autostart disabled.");
+		}
+	}
+}
