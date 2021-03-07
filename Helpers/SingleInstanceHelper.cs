565e0f0f55120b5a76ff6b48493a234258b22c69
Emma Alyx Wunder <hallo@emmalyx.dev>
2021-03-07 04:17:59 +0100

    Added check for other instances on startup

---
+++ Helpers/SingleInstanceHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading;
+
+namespace DeadEye.Helpers {
+	internal class SingleInstanceHelper {
+		private static readonly Mutex mutex = new Mutex(true, "{755d1423-1f2d-4267-a282-3f5a8ccde350}");
+
+		public static bool IsOtherInstanceRunning() {
+			return !mutex.WaitOne(TimeSpan.Zero, true);
+		}
+
+		public static void Release() {
+			mutex.ReleaseMutex();
+		}
+	}
+}
