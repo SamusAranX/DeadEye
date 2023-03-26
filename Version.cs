8c51347147c104d68515efdc8da0b5289dc4d263
Emma Alyx Wunder <hallo@emmalyx.dev>
2023-03-26 01:08:19 +0100

    Added Version class template

---
+++ Version.cs
@@ -0,0 +1,13 @@
+﻿namespace DeadEye;
+
+internal static class Version
+{
+	public const bool GENERATED = false;
+
+	public const string GIT_BRANCH = "";
+	public const string GIT_COMMIT = "";
+	public const string GIT_COMMIT_SHORT = "";
+	public const string GIT_VERSION = "";
+	public const string GIT_TAG = "";
+	public const string BUILD_TIME = "";
+}
