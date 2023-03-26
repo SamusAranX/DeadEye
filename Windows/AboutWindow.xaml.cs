b66e95937f7b6ad861e8196a80ce31f2343b6a3f
Emma Alyx Wunder <hallo@emmalyx.dev>
2023-03-26 03:30:09 +0200

    Added Pre- and Post-Build steps to insert git information into a build
    Redid AboutWindow to display git data etc

--- Windows/AboutWindow.xaml.cs
+++ Windows/AboutWindow.xaml.cs
@@ -1,44 +1,30 @@
-﻿using System.IO;
+﻿using System.Globalization;
 using System.Reflection;
 
 namespace DeadEye.Windows;
 
 public partial class AboutWindow
 {
-	private string? _appVersion;
-	private string? _gitVersion;
-
 	public AboutWindow()
 	{
 		this.InitializeComponent();
 	}
 
-	public string AppVersion
+	public DateTime BuildDateTime => DateTime.Parse(Version.BUILD_TIME, CultureInfo.InvariantCulture, DateTimeStyles.RoundtripKind).ToLocalTime();
+
+	public string Copyright
 	{
 		get
 		{
-			if (this._appVersion != null)
-				return this._appVersion;
+			var asm = Assembly.GetExecutingAssembly();
+			var obj = asm.GetCustomAttributes(false);
+			foreach (var o in obj)
+			{
+				if (o is AssemblyCopyrightAttribute aca)
+					return aca.Copyright;
+			}
 
-			var version = Assembly.GetExecutingAssembly().GetName().Version;
-			this._appVersion = version?.ToString() ?? "N/A";
-
-			return this._appVersion;
-		}
-	}
-
-	public string GitVersion
-	{
-		get
-		{
-			if (this._gitVersion != null)
-				return this._gitVersion;
-
-			using var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("DeadEye.version.txt");
-			using var reader = new StreamReader(stream ?? throw new InvalidOperationException("Couldn't load git version"));
-			this._gitVersion = reader.ReadToEnd().Trim();
-
-			return this._gitVersion;
+			return string.Empty;
 		}
 	}
 }
---
+++ scripts/gitversion.ps1
@@ -0,0 +1,33 @@
+﻿Param (
+	[String]$ProjectDir,
+	[String]$VersionFile="Version.cs"
+)
+
+$ErrorActionPreference = "Stop";
+
+Push-Location -LiteralPath $ProjectDir;
+
+$GitBranch = git rev-parse --abbrev-ref HEAD;
+$GitCommit = git rev-parse HEAD;
+$GitCommitShort = git rev-parse --short HEAD;
+$GitVersion = git describe --tags --always --dirty;
+$GitTag = git describe --tags --abbrev=0;
+$BuildTime = Get-Date (Get-Date).ToUniversalTime() -Format 'o';
+
+$NewVersionFileContent = Get-Content $VersionFile |
+    %{$_ -replace 'false', 'true' } |
+    %{$_ -replace 'GIT_BRANCH = ""', "GIT_BRANCH = `"$GitBranch`"" } |
+    %{$_ -replace 'GIT_COMMIT = ""', "GIT_COMMIT = `"$GitCommit`"" } |
+    %{$_ -replace 'GIT_COMMIT_SHORT = ""', "GIT_COMMIT_SHORT = `"$GitCommitShort`"" } |
+    %{$_ -replace 'GIT_VERSION = ""', "GIT_VERSION = `"$GitVersion`"" } |
+    %{$_ -replace 'GIT_TAG = ""', "GIT_TAG = `"$GitTag`"" } |
+    %{$_ -replace 'BUILD_TIME = ""', "BUILD_TIME = `"$BuildTime`"" };
+
+echo "Writing new $VersionFile…";
+
+$NewVersionFileContent > $VersionFile;
+# echo $NewVersionFileContent;
+
+echo "Done.";
+
+Pop-Location;
