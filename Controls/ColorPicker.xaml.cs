5eea9183a0047299698b34c8517727f03ddbe7bc
Emma Alyx Wunder <hallo@emmalyx.dev>
2023-07-29 01:28:51 +0200

    Cleaned up ColorPicker code

--- Controls/ColorPicker.xaml.cs
+++ Controls/ColorPicker.xaml.cs
@@ -8,6 +8,18 @@ namespace DeadEye.Controls;
 public partial class ColorPicker : INotifyPropertyChanged
 {
 	public const int IMAGE_SOURCE_RECT_SIZE = 15;
+	private double _pickerRadius2, _pickerRadius3;
+
+	#region INotifyPropertyChanged
+
+	public event PropertyChangedEventHandler? PropertyChanged;
+
+	private void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+	{
+		this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+	}
+
+	#endregion
 
 	public static readonly DependencyProperty PixelColorProperty = DependencyProperty.Register(
 		nameof(PixelColor),
@@ -27,41 +39,6 @@ public partial class ColorPicker : INotifyPropertyChanged
 		typeof(ColorPicker),
 		new FrameworkPropertyMetadata((double)72, FrameworkPropertyMetadataOptions.Inherits | FrameworkPropertyMetadataOptions.AffectsRender, OnPickerRadiusChanged));
 
-	private static void OnPickerRadiusChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
-	{
-		if (d is not ColorPicker picker)
-			return;
-
-		if (e.Property != PickerRadiusProperty)
-			return;
-
-		picker.PickerRadius2 = Math.Max(0, (double)e.NewValue - 1);
-		picker.PickerRadius3 = Math.Max(0, (double)e.NewValue - 4);
-
-		//Debug.WriteLine("{0} {1} {2}", picker.PickerRadius, picker.PickerRadius2, picker.PickerRadius3);
-	}
-
-	#region INotifyPropertyChanged
-
-	public event PropertyChangedEventHandler? PropertyChanged;
-
-	private void OnPropertyChanged([CallerMemberName] string? propertyName = null)
-	{
-		this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-	}
-
-	#endregion
-
-	public ColorPicker()
-	{
-		this.InitializeComponent();
-
-		this.PickerRadius2 = Math.Max(0, this.PickerRadius - 1);
-		this.PickerRadius3 = Math.Max(0, this.PickerRadius - 4);
-
-		//Debug.WriteLine("{0} {1} {2}", this.PickerRadius, this.PickerRadius2, this.PickerRadius3);
-	}
-
 	public Color PixelColor
 	{
 		get => (Color)this.GetValue(PixelColorProperty);
@@ -79,8 +56,6 @@ public partial class ColorPicker : INotifyPropertyChanged
 		get => (double)this.GetValue(PickerRadiusProperty);
 		set => this.SetValue(PickerRadiusProperty, value);
 	}
-
-	private double _pickerRadius2, _pickerRadius3;
 
 	public double PickerRadius2
 	{
@@ -100,5 +75,28 @@ public partial class ColorPicker : INotifyPropertyChanged
 			this._pickerRadius3 = value;
 			this.OnPropertyChanged();
 		}
+	}
+
+	public ColorPicker()
+	{
+		this.InitializeComponent();
+		this.UpdatePickerRadii(this.PickerRadius);
+	}
+
+	private void UpdatePickerRadii(double baseRadius)
+	{
+		this.PickerRadius2 = Math.Max(0, baseRadius - 1);
+		this.PickerRadius3 = Math.Max(0, baseRadius - 4);
+	}
+
+	private static void OnPickerRadiusChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+	{
+		if (d is not ColorPicker picker)
+			return;
+
+		if (e.Property != PickerRadiusProperty)
+			return;
+
+		picker.UpdatePickerRadii((double)e.NewValue);
 	}
 }
